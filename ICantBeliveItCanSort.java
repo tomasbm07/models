d1811b490f1b5ba6ff946881942d9909b890721e
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2022-11-16 18:42:18 +0000

    Add ICantBeliveItCanSort.java

---
+++ ICantBeliveItCanSort.java
@@ -0,0 +1,13 @@
+public class ICantBeliveItCanSort {
+    public static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] < a[j]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+    }
+}
